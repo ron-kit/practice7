29b28bbe2c2f8a70b93590c53f2a5b2affc35426
ron <rkitaini@student.ubc.ca>
2023-10-06 12:55:45 -0700

    passes all tests

--- src/main/java/cpen221practice/LuhnValidator.java
+++ src/main/java/cpen221practice/LuhnValidator.java
@@ -1,9 +1,23 @@
 package cpen221practice;
 
+import java.security.InvalidParameterException;
+
 class LuhnValidator {
 
-    boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    boolean isValid(String candidate) throws InvalidParameterException {
+        String str = candidate.replaceAll(" ", "");
+        if (str.matches("\\D") || str.length() <= 1) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int digit = Character.getNumericValue(str.charAt(i));
+            if ((i + str.length()) % 2 == 0) {
+                sum += Math.max((digit * 2) % 10, digit * 2 - 9);
+            } else {
+                sum += digit;
+            }
+        }
+        return (sum % 10 == 0);
     }
-
 }
